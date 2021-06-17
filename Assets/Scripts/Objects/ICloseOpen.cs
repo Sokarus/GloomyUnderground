08906ce9a45321e0dcb504946557d99dc87c4086
Valentine <bmaecmpo@gmail.com>
2021-06-17 21:28:26 +0500

    And another one

---
+++ Assets/Scripts/Objects/ICloseOpen.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ICloseOpen
+{
+    abstract public void Open();
+
+    abstract public void Close();
+}
